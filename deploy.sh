d44043a7b43d96a24ec7d456e6987a6833f60461
as6625998 <as6625998@gmail.com>
2023-02-24 15:17:03 +0800

    add

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://as6625998.github.io
+# git push -f git@github.com:as6625998/as6625998.github.io.git main
+
+# if you are deploying to https://as6625998.github.io/vit-deploy-simple
+git push -f https://github.com/as6625998/week6.git main:gh-pages
+cd -
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    eslintPlugin({
-      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
-    })
-  ],
+    vue()],
   base: '/week6/',
   resolve: {
     alias: {
