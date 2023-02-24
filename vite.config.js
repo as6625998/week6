314ae577a3ffec71037436b6edc2dcf9fe1a53e1
as6625998 <as6625998@gmail.com>
2023-02-24 15:46:57 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue()],
+  plugins: [vue()],
   base: '/week6/',
   resolve: {
     alias: {
