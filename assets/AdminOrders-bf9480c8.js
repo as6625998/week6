532f55f7fb64f6a06f3d190759549493f6df76bb
as6625998 <as6625998@gmail.com>
2023-02-25 19:04:40 +0800

    deploy

--- assets/AdminOrders-bf9480c8.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-70f2d5ee.js";const e={};function c(n,t){return" 這是訂單列表 "}const s=r(e,[["render",c]]);export{s as default};
--- assets/AdminProducts-1c1635fe.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-70f2d5ee.js";const c={};function t(e,n){return" 這是產品列表 "}const o=r(c,[["render",t]]);export{o as default};
